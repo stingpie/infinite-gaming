edcba350c445e965c1b17cd81d507db52342b4b8
stingpie <stingpie52@gmail.com>
2024-06-20 16:46:52 -0500

    added support for anthropic ai

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import toolcalls, tooljson, pickle, os
-from openai import OpenAI as openai
+import toolcalls, pickle, os
 
-client = openai()
+
+api="ant"
+
+if(api == "openai"):
+    from openai import OpenAI as openai
+    import tooljson_openai as tooljson
+
+    client = openai()
+elif(api == 'ant'):
+    from anthropic import Anthropic as anthropic
+    import tooljson_ant as tooljson
+
+    client = anthropic()
 
 
 if(not os.path.exists("sandbox")):
@@ -11,8 +22,10 @@ if(not os.path.exists("sandbox")):
 if(os.path.exists("chatlog.pkl")):
     log = pickle.load(open("chatlog.pkl", 'rb'))
 else:
-    log = [{"role":"system","content":open("prompt.txt",'r').read()}]
-
+    if(api == "openai"):
+        log = [{"role":"system","content":open("prompt.txt",'r').read()}]
+    else:
+        log =[]
 
 feedback =input("how would you improve this game?")
 
@@ -26,24 +39,47 @@ log.append({"role":"user", "content": feedback })
 
 
 while(stop_reason!="content_filter" and not done and count<limit):
-    completion = client.chat.completions.create(
-            model='gpt-4o',
-            messages=log,
-            tools = tooljson.tools,
-            tool_choice="required"
-    )
-    response_message = completion.choices[0].message
-    
-    log.append(response_message)
     
 
-    tool_calls = response_message.tool_calls
+    if(api =='openai'):
+        completion = client.chat.completions.create(
+                model = 'gpt-3.5-turbo',
+                messages = log,
+                tools = tooljson.tools,
+                tool_choice = 'required'
+        )
+        response_message = completion.choices[0].message
+    elif(api =='ant'):
+        completion = client.messages.create(
+                model='claude-3-5-sonnet-20240620',
+                max_tokens=4096,
+                system=open("prompt.txt",'r').read(),
+                messages=log,
+                tools = tooljson.tools,
+                tool_choice={'type':'any'}
+        )
+        response_message = completion
+
+    #response_message = completion.choices[0].message
+    if api == 'openai':
+        log.append(response_message)
+    elif api == 'ant':
+        log.append({'role':'assistant','content':response_message.content})
+    
+
+    if(api == 'openai'):
+        tool_calls = response_message.tool_calls
+    elif(api == 'ant'):
+        tool_calls = response_message.content
+        tool_content=[]
+
+
     for i in range(len(tool_calls)):
     
         tool_call_id = tool_calls[i].id
-        func_name = tool_calls[i].function.name
+        func_name = tool_calls[i].function.name if(api == 'openai') else tool_calls[i].name
         # eval creates a dictionary, since the arguments are formatted as such.
-        arguments = eval(tool_calls[i].function.arguments)
+        arguments = eval(tool_calls[i].function.arguments) if(api == 'openai') else tool_calls[i].input
 
         if(func_name == "createfile"):
             result = toolcalls.createfile(arguments)
@@ -63,15 +99,25 @@ while(stop_reason!="content_filter" and not done and count<limit):
         else:
             toolcalls.log_err("function "+func_name+" does not exist.")
 
-        log.append({'role':'tool', 'content':result, 'tool_call_id':tool_call_id, 'name':func_name})
-        
+        if(api == 'openai'):
+            log.append({'role':'tool', 'content':result, 'tool_call_id':tool_call_id, 'name':func_name})
+        elif(api == 'ant'):
+            tool_content.append({'type':'tool_result', 'tool_use_id':tool_call_id, 'content':result})
+    if(api == 'ant'):
+        log.append({'role':'user', 'content':tool_content})
+
+    print(log)
+
     count+=1
   
     print(response_message)
     
     with open('chatlog.pkl','wb') as f:
         pickle.dump( log, f)
-    stop_reason = completion.choices[0].finish_reason
-    
+
+    if(api == 'openai'):
+        stop_reason = completion.choices[0].finish_reason
+    elif(api == 'ant'):
+        stop_reason= completion.stop_reason
 
 
